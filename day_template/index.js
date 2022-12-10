e1deba17e88781661264a51a8eb4376177e0864b
Anders Jansson <tillanders@yahoo.com>
2022-12-10 21:32:01 +0100

    Day template

---
+++ day_template/index.js
@@ -0,0 +1,19 @@
+import data from "./input.js";
+import testData from "./test.js";
+import "../../helpers/helpers.js";
+
+const lines = data.split("\n");
+const testLines = testData.split("\n");
+
+const task1 = () => {};
+
+const task2 = () => {};
+
+console.log("---- task 1 ----");
+task1();
+console.log("----------------");
+console.log("");
+console.log("---- task 2 ----");
+task2();
+console.log("----------------");
+console.log("");
