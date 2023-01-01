6d6104331db56bb67c9507b843311393b4ed9918
Anders Jansson <anders.jansson@etraveligroup.com>
2023-01-01 21:57:53 +0100

    WIP - Day 16 (2022)

--- 2022/day16/index.js
+++ 2022/day16/index.js
@@ -23,72 +23,134 @@ const task1 = () => {
     {}
   );
 
-  let minute = 0;
-  let tot = 0;
-  let current = valves.AA;
-  const moves = [];
-  current.isOpen = true;
+  let minutesLeft = 30;
+  const tot = 0;
+  const state = {
+    valves,
+    position: "AA",
+    minute: 30,
+    totReleased: 0,
+    path: ["AA"],
+  };
+  let states = [state];
 
-  while (minute < 30) {
-    const currentPressure = Object.keys(valves)
-      .filter((valve) => valves[valve].isOpen)
-      .map((valve) => valves[valve].rate)
-      .sum();
-    tot += currentPressure;
+  while (minutesLeft > 0) {
+    // Only keep top 20 states
+    states.sort((a, b) => a.totReleased - b.totReleased);
+    states = states.slice(0, 20);
 
-    console.log("current", minute, currentPressure, tot, current.valve);
+    const newStates = [];
+    for (const currentState of states) {
+      const currentValve = currentState.valves[currentState.position];
+      const connections = currentValve.connections;
 
-    const allOpen = Object.keys(valves).every(
-      (valve) => valves[valve].isOpen || valves[valve].rate === 0
-    );
-    if (allOpen) {
-      console.log("all open");
-      minute++;
+      // Release pressure
+      for (const valve of currentState.valves) {
+        if (valve.isOpen) {
+          currentState.totReleased = currentState.totReleased + valve.rate;
+        }
+      }
+
+      // Open valve
+      if (!currentValve.isOpen && currentValve.rate > 0) {
+        const newValves = [...currentState.valves];
+        newValves[currentState.position] = { ...currentValve, isOpen: true };
+        newStates.push({
+          valves: newValves,
+          position: currentState.position,
+          minute: currentState.minute - 1,
+          totReleased: currentState.totReleased,
+          path: currentState.path,
+        });
+      }
+
+      // Go to connections
+      for (const connection of connections) {
+        // Go to connection
+        newStates.push({
+          valves: currentState.valves,
+          position: connection,
+          minute: currentState.minute - 1,
+          totReleased: currentState.totReleased,
+          path: [...currentState.path, connection],
+        });
+      }
+    }
+    states = newStates;
+    minutesLeft--;
+  }
+
+  console.log(tot);
+};
+
+const task1_DP = () => {
+  const valves = testLines.reduce(
+    (acc, line) => ({
+      ...acc,
+      [line.split(" ")[1]]: {
+        isOpen: false,
+        valve: line.split(" ")[1],
+        rate: Number(line.split(" ")[4].split("=")[1].split(";")[0]),
+        connections: line
+          .split(" valv")[1]
+          .split(" ")
+          .slice(1)
+          .map((v) => v.replaceAll("\n", "").replaceAll(",", "")),
+      },
+    }),
+    {}
+  );
+
+  const tot = 0;
+  const state = {
+    valves,
+    position: "AA",
+    minute: 30,
+    totReleased: 0,
+    path: ["AA"],
+  };
+  const states = [state];
+
+  while (states.length > 0) {
+    const currentState = states.shift();
+    const currentValve = currentState.valves[currentState.position];
+    const connections = currentValve.connections;
+
+    // Release pressure
+    for (const valve of currentState.valves) {
+      if (valve.isOpen) {
+        currentState.totReleased = currentState.totReleased + valve.rate;
+      }
+    }
+
+    // Check if best
+    if (currentState.minute === 0) {
       continue;
     }
 
-    const bestMove = Object.keys(valves)
-      .filter((key) => {
-        return !valves[key].isOpen && valves[key].rate !== 0;
-      })
-      .reduce(
-        (best, valveKey) => {
-          if (valveKey === current.valve) return best;
-          const valve = valves[valveKey];
-          const shortestPathToValve = djikstra(
-            Object.keys(valves).map((key) => ({
-              value: key,
-              connections: valves[key].connections,
-            })),
-            { value: current.valve, connections: current.connections },
-            [valveKey]
-          );
-          if (best.ppm < valve.rate / shortestPathToValve.cost) {
-            return {
-              ppm: valve.rate / shortestPathToValve.cost,
-              moves: shortestPathToValve.path.map((node) => node.value).slice(1),
-            };
-          }
-          return best;
-        },
-        { ppm: -1, moves: [] }
-      );
-    //console.log("best", bestMove);
-
-    if (moves.length === 0 && (current.isOpen || bestMove.ppm > current.rate)) {
-      moves.push(...bestMove.moves);
+    // Open valve
+    if (!currentValve.isOpen && currentValve.rate > 0) {
+      const newValves = [...currentState.valves];
+      newValves[currentState.position] = { ...currentValve, isOpen: true };
+      states.push({
+        valves: newValves,
+        position: currentState.position,
+        minute: currentState.minute - 1,
+        totReleased: currentState.totReleased,
+        path: currentState.path,
+      });
     }
 
-    if (moves.length > 0) {
-      // Move
-      console.log("move", moves[0]);
-      minute++;
-      current = valves[moves.shift()];
-    } else {
-      console.log("open", current.valve);
-      // Open
-      minute++;
-      current.isOpen = true;
+    // Go to connections
+    for (const connection of connections) {
+      // Go to connection
+      states.push({
+        valves: currentState.valves,
+        position: connection,
+        minute: currentState.minute - 1,
+        totReleased: currentState.totReleased,
+        path: [...currentState.path, connection],
+      });
     }
   }
 
